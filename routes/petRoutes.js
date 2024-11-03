4e4ad7c9f418d23dbd60850f6c918a345a7133df
Kum yew <lokekumyew@gmail.com>
2024-11-03 10:38:32 +0700

    Added functionality to reset pet/pets with auth

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -18,6 +18,14 @@ const authenticateToken = (req, res, next) => {
     });
 };
 
+const verifyResetApiKey = (req, res, next) => {
+    const apiKey = req.headers['x-api-key'];
+    if (apiKey !== process.env.RESET_API_KEY) {
+        return res.status(403).json({ error: 'Unauthorized access' });
+    }
+    next();
+};
+
 //Function to update pet states
 const updatePetStates = async () => {
     console.log('Updating pet states...');
@@ -86,6 +94,42 @@ router.post('/', async (req, res) => {
     const newPet = new Pet({ petType:petType, userId: userId }); 
     await newPet.save();
     res.status(201).json({ status: 'success', data: newPet });;
+});
+
+//Reset all pets but need authentication
+router.post('/reset-pet-states/all', verifyResetApiKey, async (req, res) => {
+    console.log('Resetting all pets...');
+    try {
+        await Pet.updateMany({}, { hunger: 0, health: 100, happiness: 100, poopCount: 0, isAlive:true, lastUpdated: Date.now() });
+        console.log('All pet states have been reset.');
+        res.status(200).json({ message: 'All pet states have been reset.' });
+    } catch (error) {
+        console.error('Error resetting pet states:', error);
+        res.status(500).json({ error: 'Failed to reset pet states.' });
+    }
+});
+
+// Reset a pet by id
+router.post('/reset-pet-states/:id', verifyResetApiKey, async (req, res) => {
+    console.log('Resetting pet state...');
+    try {
+        const pet = await Pet.findById(req.params.id);
+        if (!pet) {
+            return res.status(404).json({ error: 'Pet not found' });
+        }
+        pet.hunger = 0;
+        pet.health = 100;
+        pet.happiness = 100;
+        pet.poopCount = 0;
+        pet.isAlive = true;
+        pet.lastUpdated = Date.now();
+        await pet.save();
+        console.log('Pet state has been reset.');
+        res.status(200).json({ message: 'Pet state has been reset.' });
+    } catch (error) {
+        console.error('Error resetting pet state:', error);
+        res.status(500).json({ error: 'Failed to reset pet state.' });
+    }
 });
 
 // Get a pet by userId
